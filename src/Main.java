afe8d19dba539cd425ed22eebcce6d268d9144d7
Logstor <a.rottger_rydahl@live.dk>
2018-11-13 14:40:30 +0100

    Created Main and print Hello World

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Alfred Röttger Rydahl
+ * @date 13-11-2018
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
